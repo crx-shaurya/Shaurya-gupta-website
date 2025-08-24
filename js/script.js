634c0653b69c2d4c1d0b26588354f77d1483e5e8
Shaurya Gupta <dhruvgupta9411@gmail.com>
2025-08-24 16:54:51 +0530

    4

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Custom Cursor Movement
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
+// Smooth Scroll
+window.scrollTo({ top: 0, behavior: "smooth" });
